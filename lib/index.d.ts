5e0357a382c8c1da9a23612afb99d7f504ca79ab
CinderD <63868789+CinderD@users.noreply.github.com>
2021-08-03 02:28:52 +0800

    Add files via upload

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+import { JupyterFrontEndPlugin } from '@jupyterlab/application';
+/**
+ * Initialization data for the jupyterlab-play-cell-button extension.
+ */
+declare const extension: JupyterFrontEndPlugin<void>;
+export default extension;
